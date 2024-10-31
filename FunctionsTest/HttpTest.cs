cb9449cc623a6802ac4755200c49ebdb0c604bbc
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2024-10-31 22:55:50 +0100

    added HttpTest.cs

---
+++ FunctionsTest/HttpTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunctionsTest;
+
+public class HttpTest
+{
+    private readonly ILogger<HttpTest> _logger;
+
+    public HttpTest(ILogger<HttpTest> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpTest")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+        
+    }
+
+}
